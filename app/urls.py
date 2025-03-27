488b1f79688c6ddc9d214d65fe66f98d2c68004a
Caiera02 <caiocezar2807@gmail.com>
2025-03-27 20:57:28 -0300

    add login e register

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from formulario.views import avaliacao_view,teste_view
+from formulario.views import avaliacao_view
+from acconunts.views import register_view,login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teste/',teste_view, name='pagina_de_sucesso'),
+    path('register/',register_view, name='register'),
+    path('login/',login_view, name='login'),
     path('avaliacao/', avaliacao_view, name='new_avaliacao'),
 ]
