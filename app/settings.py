9d7b5c951e914a9bc2faa3557f8d51d28de11a25
Caiera02 <caiocezar2807@gmail.com>
2025-03-28 14:06:39 -0300

    add new app ra

--- app/settings.py
+++ app/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'formulario',
     'acconunts',
+    'ra',
 ]
 
 MIDDLEWARE = [
---
+++ ra/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name= 'Colaborador'
+        
+    def __str__(self):
+        return self.nome
+
+class Criterio(models.Model):
+    nome = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+
+class Avaliacao(models.Model):
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE)
+    criterio = models.ForeignKey(Criterio,on_delete=models.CASCADE)
+    nota = models.IntegerField(choices=[(i, i) for i in range(1, 6)])# Fazer aparecer apenas de 1 a 5
+    
+    class Meta:
+        unique_together = ('colaborador', 'criterio') 
